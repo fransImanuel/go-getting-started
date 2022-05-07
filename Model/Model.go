1ed51054295cebd50d1ddbebe455d482e70f0971
fransimanue <frans.imanuel@student.umn.ac.id>
2022-05-08 00:52:56 +0700

    a

--- Model/Model.go
+++ Model/Model.go
@@ -5,9 +5,9 @@ type Guitars struct {
 	Brand_ID    *int     `json:"Brand_ID,omitempty"  db:"Brand_Id"`
 	Guitar_Name *string  `json:"Guitar_Name,omitempty"  db:"Name"`
 	Price       *float64 `json:"Price,omitempty"  db:"Price"`
-	Back        *int     `json:"Back,omitempty"  db:"Back"`
-	Side        *int     `json:"Side,omitempty"  db:"Side"`
-	Neck        *int     `json:"Neck,omitempty"  db:"Neck"`
+	Back        *float64     `json:"Back,omitempty"  db:"Back"`
+	Side        *float64     `json:"Side,omitempty"  db:"Side"`
+	Neck        *float64     `json:"Neck,omitempty"  db:"Neck"`
 	GuitarSize  *int     `json:"GuitarSize,omitempty"  db:"GuitarSize"`
 	Description *string  `json:"Description,omitempty"  db:"Description"`
 	Image       *string  `json:"Image,omitempty"  db:"Image"`
